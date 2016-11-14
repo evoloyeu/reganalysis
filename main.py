bc5967ebf81e283d14eb2715666c048f5dba482d
rexlei <rexlei@rexlei.local>
2016-11-14 13:54:16 -0800

    add split function in main; list 3rd, 4th year courses list and their counts

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 
 import regrmdup, rawData, sys, MTIS, Validate
 
-validate, mtis = False, False
+validate, mtis, split = True, False, False
 
 if validate:
 	Validate.myValidate(sys.argv).doBatch()
 elif mtis:
 	# build MTIS data
 	MTIS.myMTIS(sys.argv).matrixBuilder()
+elif split:
+	prepare = rawData.splitRawData(sys.argv)
+	prepare.doBatch()
 else:
 	# compute thesis data
 	prepare = rawData.splitRawData(sys.argv)
