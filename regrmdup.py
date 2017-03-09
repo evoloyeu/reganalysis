8125c365cf5d610bac9cd791c1948aaaf3345884
rexlei <life545@163.com>
2017-03-09 03:26:29 -0800

    bugfix

--- regrmdup.py
+++ regrmdup.py
@@ -1416,8 +1416,8 @@ class prepross(object):
 					r, slope, intercept, r_value, p_value, std_err = [float(format(r, '.4f')), float(format(slope, '.4f')), float(format(intercept, '.4f')), float(format(r_value, '.4f')), float(format(p_value, '.4f')), float(format(std_err, '.4f'))]
 
 					# r_value = float(format(r, '.4f'))
-					slope, intercept = self.regressionPlot(xdata, ydata, r_value, 1, xaxis, yaxis, self.linear_plots_ori, len(ydata))
-					a,b,c = self.regressionPlot(xdata, ydata, r_value, 2, xaxis, yaxis, self.quadratic_plots_ori, len(ydata))
+					slope, intercept = self.regressionPlot(xdata, ydata, r, 1, xaxis, yaxis, self.linear_plots_ori, len(ydata))
+					a,b,c = self.regressionPlot(xdata, ydata, r, 2, xaxis, yaxis, self.quadratic_plots_ori, len(ydata))
 
 					slope, intercept, a, b, c = [float(format(slope, '.4f')), float(format(intercept, '.4f')), float(format(a, '.4f')), float(format(b, '.4f')), float(format(c, '.4f'))]
 
@@ -1608,8 +1608,8 @@ class prepross(object):
 				r, slope, intercept, r_value, p_value, std_err = [float(format(r, '.4f')), float(format(slope, '.4f')), float(format(intercept, '.4f')), float(format(r_value, '.4f')), float(format(p_value, '.4f')), float(format(std_err, '.4f'))]
 
 				# r_value = float(format(r, '.4f'))
-				slope, intercept = self.regressionPlot(xdata, ydata, r_value, 1, xaxis, yaxis, self.linear_plots_ori, len(ydata))
-				a,b,c = self.regressionPlot(xdata, ydata, r_value, 2, xaxis, yaxis, self.quadratic_plots_ori, len(ydata))
+				slope, intercept = self.regressionPlot(xdata, ydata, r, 1, xaxis, yaxis, self.linear_plots_ori, len(ydata))
+				a,b,c = self.regressionPlot(xdata, ydata, r, 2, xaxis, yaxis, self.quadratic_plots_ori, len(ydata))
 
 				slope, intercept, a, b, c = [float(format(slope, '.4f')), float(format(intercept, '.4f')), float(format(a, '.4f')), float(format(b, '.4f')), float(format(c, '.4f'))]
 
